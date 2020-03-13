efb01a8f08e6cbb9cd8bb358f15b55183f81cab5
zigihouse <zigihouse@gmail.com>
2020-03-13 16:25:36 +0200

    Update logger

--- app.rb
+++ app.rb
@@ -1,10 +1,20 @@
 class Logger
+  def initialize
+    @f = File.open 'log.txt', 'a'
+  end
+
+# class method
   def self.say_something
     puts "Haha!"
+  end
+
+# instance method
+  def self.log_something what
+    @f.puts what
   end
 end
 
 Logger.say_something
 
-# logger = Logger.new
-# logger.say_something
+logger = Logger.new
+logger.log_something 'hey!'
